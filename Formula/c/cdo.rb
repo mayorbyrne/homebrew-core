6c9921a6c20a874790d737c02379808d7b6bf98c
Rui Chen <rui@chenrui.dev>
2024-05-07 09:18:08 -0400

    cdo: revision bump (hdf5 1.14.4)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cdo.rb
+++ Formula/c/cdo.rb
@@ -4,6 +4,7 @@ class Cdo < Formula
   url "https://code.mpimet.mpg.de/attachments/download/29646/cdo-2.4.4.tar.gz"
   sha256 "fc00a71dc83d9c90b172f08e0ae71e1fd3162760461600cd25cf0dfb78ff9453"
   license "GPL-2.0-only"
+  revision 1
 
   livecheck do
     url "https://code.mpimet.mpg.de/projects/cdo/files"
