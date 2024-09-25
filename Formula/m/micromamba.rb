1cf407fa5d0b8b9ff9ff97b72d060453041d5029
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-25 15:44:47 +0000

    micromamba 2.0.0
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/m/micromamba.rb
+++ Formula/m/micromamba.rb
@@ -1,8 +1,8 @@
 class Micromamba < Formula
   desc "Fast Cross-Platform Package Manager"
   homepage "https://github.com/mamba-org/mamba"
-  url "https://github.com/mamba-org/mamba/archive/refs/tags/micromamba-1.5.10.tar.gz"
-  sha256 "38ee4658f66c5e4bf2c33cd3c9c0ebd01fe2e3a6da6ac619cc4702a9072dcc3c"
+  url "https://github.com/mamba-org/mamba/archive/refs/tags/micromamba-2.0.0.tar.gz"
+  sha256 "ea6f29327a7877abb79a1e8a16af2f4a4db4ae84c196bc159205b411db8c7dac"
   license "BSD-3-Clause"
   head "https://github.com/mamba-org/mamba.git", branch: "main"
 
@@ -34,43 +34,57 @@ class Micromamba < Formula
   depends_on "lz4"
   depends_on "openssl@3"
   depends_on "reproc"
+  depends_on "simdjson"
   depends_on "xz"
   depends_on "yaml-cpp"
   depends_on "zstd"
 
+  uses_from_macos "llvm" => :build, since: :sonoma
   uses_from_macos "python" => :build
   uses_from_macos "bzip2"
   uses_from_macos "curl", since: :ventura # uses curl_url_strerror, available since curl 7.80.0
   uses_from_macos "krb5"
   uses_from_macos "zlib"
 
+  # /usr/include/c++/11/bits/stl_pair.h:218:11: error: ‘std::pair<_T1, _T2>::second’ has incomplete type
+  #   218 |       _T2 second;                ///< The second member
+  #       |           ^~~~~~
+  # Might work with a newer GCC, but we don't want linkage with a newer `libstdc++`.
+  fails_with :gcc
+
   def install
+    ENV.llvm_clang if OS.mac? && MacOS.version < :sonoma
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
+
     args = %W[
       -DBUILD_LIBMAMBA=ON
       -DBUILD_SHARED=ON
-      -DBUILD_MICROMAMBA=ON
-      -DMICROMAMBA_LINKAGE=DYNAMIC
+      -DBUILD_MAMBA=ON
       -DCMAKE_INSTALL_RPATH=#{rpath}
     ]
 
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
+    # Upstream only want to call the statically linked executable `micromaba`,
+    # but let's not break existing users of this formula.
+    bin.install_symlink "mamba" => "micromamba"
   end
 
   def caveats
     <<~EOS
       Please run the following to setup your shell:
-        #{opt_bin}/micromamba shell init -s <your-shell> -p ~/micromamba
+        #{opt_bin}/mamba shell init -s <your-shell> -p ~/mamba
       and restart your terminal.
     EOS
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/micromamba --version").strip
+    ENV["MAMBA_ROOT_PREFIX"] = testpath.to_s
+    assert_match version.to_s, shell_output("#{bin}/mamba --version").strip
 
     python_version = "3.9.13"
-    system bin/"micromamba", "create", "-n", "test", "python=#{python_version}", "-y", "-c", "conda-forge"
-    assert_match "Python #{python_version}", shell_output("#{bin}/micromamba run -n test python --version").strip
+    system bin/"mamba", "create", "-n", "test", "python=#{python_version}", "-y", "-c", "conda-forge"
+    assert_match "Python #{python_version}", shell_output("#{bin}/mamba run -n test python --version").strip
   end
 end
